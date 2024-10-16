bab99bb8c7f240ed40d9979adbec53c1f29cdb1d
nrplondon <nathaniel.nuamah@nrpltd.com>
2024-10-16 13:49:43 +0100

    Add files via upload

---
+++ data/TemporaryTrafficLight_1.js
@@ -0,0 +1,8 @@
+var json_TemporaryTrafficLight_1 = {
+"type": "FeatureCollection",
+"name": "TemporaryTrafficLight_1",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "id": null, "Lights": "Temporary Traffic Lights" }, "geometry": { "type": "Point", "coordinates": [ -0.164758479523104, 51.534167463937592 ] } }
+]
+}
