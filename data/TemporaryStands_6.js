cde2d840df9e6491c37c409a01ee05c50bacfd83
nrplondon <nathaniel.nuamah@nrpltd.com>
2024-10-23 21:22:31 +0100

    Add files via upload

---
+++ data/TemporaryStands_6.js
@@ -0,0 +1,9 @@
+var json_TemporaryStands_6 = {
+"type": "FeatureCollection",
+"name": "TemporaryStands_6",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "id": null }, "geometry": { "type": "Point", "coordinates": [ -0.172238018427908, 51.521349559367259 ] } },
+{ "type": "Feature", "properties": { "id": null }, "geometry": { "type": "Point", "coordinates": [ -0.193865671752583, 51.534442171856689 ] } }
+]
+}
